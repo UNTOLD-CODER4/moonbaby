be85cd8d378e484fa40a2257d57b4e78d9b7991e
kakshi0962z <101088760+kakshi0962z@users.noreply.github.com>
2024-09-04 21:20:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/MOON-CODES/InflexMusicBot",
+    "https://github.com/kakshi0962z/moonbaby",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
