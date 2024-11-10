265a3eb8ae77420cc5a52b8fbe247cc8d2bd5c29
UNTOLD CODER <101088760+UNTOLD-CODER4@users.noreply.github.com>
2024-11-10 21:57:08 +0530

    Update welcome.py

--- InflexMusic/plugins/tools/welcome.py
+++ InflexMusic/plugins/tools/welcome.py
@@ -17,7 +17,7 @@ from pyrogram.enums import ParseMode
 from pyrogram import *
 from pyrogram.types import *
 from logging import getLogger
-from InflexMusic.utils.__init__.py import admin_filter
+from InflexMusic.utils.jarvis_ban import admin_filter
 from PIL import ImageChops
 
 LOGGER = getLogger(__name__)
